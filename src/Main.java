351526b0093bce2f3eb56565ed5f84c409f07a99
lina mayakovskaya <linamayakovskaya@mail.ru>
2023-06-15 19:14:51 +0500

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int initialAccount = 0; //начальный счёт
+        int replenishmentAmount = 1300; //сумма пополнения
+        int rublesPerRuble = 100; //количество рублей для одного бонусного рубля
+
+        int bonus;
+        int account;
+        if (replenishmentAmount > 1000) {
+            bonus = (replenishmentAmount / 100);
+            account = (initialAccount + replenishmentAmount + bonus);
+        } else {
+            bonus = 0;
+            account = initialAccount + replenishmentAmount;
+        }
+
+        System.out.println("Количество бонусных рублей:" + bonus);
+        System.out.println("Итоговый счёт:" + account);
+
     }
 }
